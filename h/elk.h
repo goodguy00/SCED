c15c5cb4b9c301a0cfcb7c68adefbb4236e5913d
goodguy00 <goodguy00@zoho.com>
2017-07-12 22:23:55 +0200

    Update elk.h

--- h/elk.h
+++ h/elk.h
@@ -1,9 +1,7 @@
 /*
- * $Header: /home/barad-dur/vision/forsyth/schenney/sced-0.94/h/RCS/elk.h,v 1.0 1997/05/06 20:41:44 schenney Exp $
+ * $Header:  $
  *
  * $Log: elk.h,v $
- * Revision 1.0  1997/05/06 20:41:44  schenney
- * Initial revision
  *
  */
 #ifndef _ELK_H_
